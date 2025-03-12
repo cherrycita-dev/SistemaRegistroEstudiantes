e0f2ee10e9d680100adc176b843bc5ba9452847f
kvalencee <kevalenciahd09@gmail.com>
2025-03-11 21:03:28 -0600

    Agregar estudiante

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Estudiante[] estudiantes = new Estudiante[100];
+    static int numEstudiantes = 0;
+
     static void menu() {
         System.out.println("Gestor de estudiantes");
         System.out.println("Seleccione una opción:");
@@ -13,7 +16,23 @@ public class Main {
     }
 
     static void addStudent() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Edad: ");
+        int edad = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el buffer
+        System.out.print("Matrícula: ");
+        String matricula = scanner.nextLine();
+        System.out.print("Carrera: ");
+        String carrera = scanner.nextLine();
+
+        Estudiante nuevoEstudiante = new Estudiante(nombre, edad, matricula, carrera);
+        estudiantes[numEstudiantes] = nuevoEstudiante;
+        numEstudiantes++;
+
+        System.out.println("Estudiante registrado con éxito.");
     }
 
     static void showStudents() {
@@ -53,5 +72,19 @@ public class Main {
             menu();
             option = scanner.nextInt();
         }
+    }
+}
+
+class Estudiante {
+    String nombre;
+    int edad;
+    String matricula;
+    String carrera;
+
+    public Estudiante(String nombre, int edad, String matricula, String carrera) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.matricula = matricula;
+        this.carrera = carrera;
     }
 }
